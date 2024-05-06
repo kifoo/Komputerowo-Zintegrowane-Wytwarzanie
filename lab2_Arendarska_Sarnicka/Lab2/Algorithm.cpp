ec3c9762b25785b4d1b6aa7db4c696ce2548c8f4
Kifoo <magdaa2622@gmail.com>
2024-05-06 11:43:20 +0200

    Add TabuSearch and clean code

--- lab2_Arendarska_Sarnicka/Lab2/Algorithm.cpp
+++ lab2_Arendarska_Sarnicka/Lab2/Algorithm.cpp
@@ -67,6 +67,75 @@ int Algorithm::dummyCode() {
 	return calculatePenalty(task_list);;
 }
 
+// =====================================
+/* Create neighbor permutations */
+vector<vector<Element>> Algorithm::permutations(vector<Element> t_list) {
+	vector<vector<Element>> all_permutations;
+	for (size_t i = 0; i < t_list.size(); i++) {
+		for (size_t j = i + 1; j < t_list.size(); j++) {
+			vector<Element> tmp = t_list;
+			swap(tmp[i], tmp[j]);
+			all_permutations.push_back(tmp);
+		}
+	}
+	return all_permutations;
+}
+
+/* Tabu Search */
+int Algorithm::tabuSearch(int max_iter, int penalty) {
+	int actual_penalty = calculatePenalty(task_list);
+	vector<Element> best_list = this->task_list;
+	vector<Element> actual_list = this->task_list;
+
+	vector<vector<Element>> tabuList;
+
+	for (int iter = 0; iter < max_iter; iter++) {
+		//Create permutations of actual_list 
+		vector<vector<Element>> all_permutations = permutations(actual_list);
+		vector<Element> best_permutation;
+
+		for (vector<Element>& neighbor : all_permutations) {
+			// Check if all_permutation has new lists 
+			if (find_if(tabuList.begin(), tabuList.end(),
+				[&neighbor](const vector<Element>& v)
+				{return comparator_equal(v, neighbor); }) == tabuList.end()) {
+				int penalty = calculatePenalty(neighbor);
+				// Check if new list has better penalty than actual_penalty  
+				if (penalty < actual_penalty) {
+					best_permutation = neighbor;
+					actual_penalty = penalty;
+				}
+			}
+		}
+
+		// If not better permutation than break
+		if (best_permutation.empty()) {
+			break;
+		}
+
+		// Update actual_list and tabuList
+		actual_list = best_permutation;
+		tabuList.push_back(best_permutation);
+		if (tabuList.size() > 12) {
+			// Removing the oldest list 
+			tabuList.erase(tabuList.begin());
+		}
+
+		// Update best_list if best_permutation is better 
+		if (calculatePenalty(best_permutation) < calculatePenalty(best_list)) {
+			best_list = best_permutation;
+			penalty = calculatePenalty(best_list);
+			// Update result_list
+			this->result_list.clear();
+			for (const auto& task : best_list) {
+				this->result_list.push_back(task.id);
+			}
+		}
+	}
+	return penalty;
+}
+// =====================================
+
 // Read data from file
 void Algorithm::read_data(const string& path) {
 	ifstream data;
--- lab2_Arendarska_Sarnicka/Lab2/Lab2.cpp
+++ lab2_Arendarska_Sarnicka/Lab2/Lab2.cpp
@@ -37,46 +37,40 @@ void write_data(const string& path, const vector<Algorithm>& algorytmy, string t
 int main()
 {
 	const string data_p[11] = { "data/p10.txt", "data/p11.txt", "data/p12.txt", "data/p13.txt", "data/p14.txt", "data/p15.txt", "data/p16.txt", "data/p17.txt", "data/p18.txt", "data/p19.txt", "data/p20.txt" };
-	const string data_s[2] = { "data/solutions.txt", "data/solutions2.txt" };
+	const string data_s[3] = { "data/solutions.txt", "data/solutions2.txt", "data/solutions3.txt" };
 	vector<Algorithm> algorytmy;
 	vector<Algorithm> algorytmy2;
-	int sum1 = 0, sum2 =0;
+	vector<Algorithm> algorytmy3;
+	int sum1 = 0, sum2 = 0, sum3 = 0;
 	for (int i = 0; i < 11; i++)
 	{
-		Algorithm alg1, alg2;
+		Algorithm alg1, alg2, alg3;
 		alg1.read_data(data_p[i]);
 		alg2 = alg1;
+		alg3 = alg1;
 		int minPenalty1 = alg1.dynamicIteration();
-		int minPenalty2 = alg2.dummyCode();
+		int minPenalty2 = alg2.tabuSearch(100, INT_MAX);
+		int minPenalty3 = alg3.dummyCode();
 		//cout << "File: " << data_p[i] << endl;
 		//cout << "Minimum Penalty using Dynamic Iteration: " << minPenalty << endl;
 
 		alg1.penalty_sum = minPenalty1;
+		alg2.penalty_sum = minPenalty2;
+		alg3.penalty_sum = minPenalty3;
 		algorytmy.push_back(alg1);
 		algorytmy2.push_back(alg2);
+		algorytmy3.push_back(alg3);
 		sum1 += minPenalty1;
 		sum2 += minPenalty2;
+		sum3 += minPenalty3;
 	}
 	cout << "Suma kar 1: " << sum1 << endl;
 	cout << "Suma kar 2: " << sum2 << endl;
+	cout << "Suma kar 3: " << sum3 << endl;
 	write_data(data_s[0], algorytmy, "\t*****\nDynamic Iterations\n\t*****");
-	write_data(data_s[1], algorytmy2, "\t*****\nDummy Code\n\t*****");
-
-	//for (int i = 0; i < 11; i++)
-	//{
-	//	Algorithm alg;
-	//	alg.read_data(data_p[i]);
-	//	int minPenalty2 = alg.dummyCode();
-
-	//	//cout << "File: " << data_p[i] << endl;
-	//	//cout << "Minimum Penalty using Dummy Code: " << minPenalty << endl;
-
-	//	algorytmy2.push_back(alg);
-	//	sum += minPenalty2;
-	//}
-	//write_data(data_s[1], algorytmy2, "\t*****\nDummy Code\n\t*****");
+	write_data(data_s[1], algorytmy2, "\n\t*****\nTabu Search\n\t*****");
+	write_data(data_s[2], algorytmy3, "\n\t*****\nDummy Code\n\t*****");
 }
-
 
 
 
